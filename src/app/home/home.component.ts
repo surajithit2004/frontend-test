013649699e34da7be3ac6507c8fbd10c4c243296
surajithit2004 <surajithit2004@gmail.com>
2019-12-11 17:15:56 +0100

    New home files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemsService } from '../service/items.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  pageTitle = 'Items';
+  _listFilter = '';
+  items: any[] = [];
+  filteredItems: any[] = [];
+  errorMessage = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredItems = this.listFilter ? this.performFilter(this.listFilter) : this.items;
+  }
 
-  constructor() { }
+  constructor(private itemsService: ItemsService) { }
 
-  ngOnInit() {
+  performFilter(filterBy: string): any[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.items.filter((item: any) =>
+      item.title.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  ngOnInit(): void {
+    this.itemsService.getAllItems().subscribe({
+      next: items => {
+        this.items = items;
+        this.filteredItems = items;
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
 }
