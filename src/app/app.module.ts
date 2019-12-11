9a50f4d8c983cb9f307b92caf9d08d2414a40c62
surajithit2004 <surajithit2004@gmail.com>
2019-12-11 17:14:50 +0100

    module file
    App module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { SearchFilterPipe } from './search-filter.pipe';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, SearchFilterPipe, ItemDetailComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule,AlertModule.forRoot(), Ng2SearchPipeModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
