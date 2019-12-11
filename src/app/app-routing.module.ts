42c302d24c5e47df70a905f32641e5256e11e702
surajithit2004 <surajithit2004@gmail.com>
2019-12-11 17:12:22 +0100

    Added routing file
    routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'items',
     component: HomeComponent
+  },
+  {
+    path: 'item-details/:id',
+    component: ItemDetailComponent
+  },
+  {
+    path: '',
+    redirectTo: 'items',
+    pathMatch: 'full'
   }
 ];
 
